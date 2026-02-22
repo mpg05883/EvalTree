f55f5a13e929810f4054a2b6acde0a356a854e23
Mike Gee <mpgee@usc.edu>
2026-02-21 20:29:43 -0800

    Deleted extracted node analysis results because file names were too long

--- EvalTree/WeaknessProfile/ExtractedNode_Analysis/results/figure.sh
+++
@@ -1,7 +0,0 @@
-for dataset in MATH MMLU DS-1000; do
-    python -m EvalTree.WeaknessProfile.ExtractedNode_Analysis.results.figure \
-        --predictor_dataset $dataset --target_dataset $dataset
-done
-
-python -m EvalTree.WeaknessProfile.ExtractedNode_Analysis.results.figure \
-    --predictor_dataset MATH --target_dataset CollegeMath
